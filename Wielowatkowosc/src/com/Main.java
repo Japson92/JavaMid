3c264f5da5f78854d8ba8fc9a542133a6c85ad7b
japson92 <sebastianlefek92@gmail.com>
2022-03-19 21:56:24 +0100

    fixedthreadpool - executor wielowatkowy

--- Wielowatkowosc/src/com/Main.java
+++ Wielowatkowosc/src/com/Main.java
@@ -1,5 +1,6 @@
 package com;
 
+import java.sql.Time;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,10 +10,49 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Główny wątek apki: " + Thread.currentThread().getName());
-       /* Thread thread = new MyThread("My-Thread-1");
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Runnable worker1 = () -> {
+            try {
+                System.out.println("Robotnik 1 | Aktualny wątek : " + Thread.currentThread().getName());
+                System.out.println("Laduje butle z tlenem. ");
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
 
-        thread.start();*/
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Runnable worker2 = () -> {
+            try {
+                System.out.println("Robotnik 2 | Aktualny wątek : " + Thread.currentThread().getName());
+                System.out.println("Laduje zapas pozywienia. ");
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable worker3 = () -> {
+            try {
+                System.out.println("Robotnik 3 | Aktualny wątek : " + Thread.currentThread().getName());
+                System.out.println("Laduje paliwo. ");
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        executor.submit(worker1);
+        executor.submit(worker2);
+        executor.submit(worker3);
+        executor.shutdown();
+
+
+
+
+
+
+        /*       *//* Thread thread = new MyThread("My-Thread-1");
+
+        thread.start();*//*
+
         Runnable countdown = () -> {
             try {
                 System.out.println(" | Wykonywany wątek (countdown) : " + Thread.currentThread().getName());
@@ -25,22 +65,18 @@ public class Main {
                 e.printStackTrace();
             }
         };
-
-
-
-
         Runnable blastOff = () -> {
             System.out.println(" | Wykonywany wątek (countdown) : " + Thread.currentThread().getName());
             System.out.println("Blast off!");
         };
-       /* Thread countdownTread = new Thread(countdown, "Countdown");
+       *//* Thread countdownTread = new Thread(countdown, "Countdown");
         Thread blastOfThread = new Thread(blastOff, "Blast off!");
         countdownTread.start();
         countdownTread.join();
-        blastOfThread.start();*/
+        blastOfThread.start();*//*
         executor.submit(countdown);
         executor.submit(blastOff);
-        executor.shutdown();
+        executor.shutdown();*/
 
     }
 }

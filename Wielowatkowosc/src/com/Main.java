ca9f53ed8b2b22b35d55f71479499fc6bf0bc314
japson92 <sebastianlefek92@gmail.com>
2022-03-19 15:37:58 +0100

    Tworzenie nowego wątku - klasa Thread

--- Wielowatkowosc/src/com/Main.java
+++ Wielowatkowosc/src/com/Main.java
@@ -3,6 +3,10 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Główny wątek apki: " + Thread.currentThread().getName());
+        Thread thread = new MyThread("My-Thread-1");
+        Thread secondThread = new MyThread("My-Thread-2");
+        thread.start();
+        secondThread.start();
     }
 }
---
+++ Wielowatkowosc/src/com/MyThread.java
@@ -0,0 +1,14 @@
+package com;
+
+import java.util.stream.IntStream;
+
+public class MyThread extends Thread{
+
+    public MyThread (String name){
+        super(name);
+    }
+    @Override
+    public void run(){
+        IntStream.rangeClosed(1,20).forEach(i -> System.out.println(i + " | Wykonywany wątek: " + Thread.currentThread().getName()));
+    }
+}

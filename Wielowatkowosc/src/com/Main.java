48cfb0ed4a172c6dfa0d2a953fe31faa45c6a6a9
japson92 <sebastianlefek92@gmail.com>
2022-03-19 15:50:22 +0100

    Tworzenie nowego wątku - klasa Runnable

--- Wielowatkowosc/src/com/Main.java
+++ Wielowatkowosc/src/com/Main.java
@@ -5,8 +5,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Główny wątek apki: " + Thread.currentThread().getName());
         Thread thread = new MyThread("My-Thread-1");
-        Thread secondThread = new MyThread("My-Thread-2");
+
         thread.start();
-        secondThread.start();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(" | Wykonywany wątek (runable): " + Thread.currentThread().getName());
+            }
+        };
+        Thread anotherthread = new Thread(runnable, "My-Runnable-1");
+        anotherthread.start();
+
     }
 }
